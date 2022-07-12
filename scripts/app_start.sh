88a2891d03ca9b536d5a360559daade0d6803a2b
solihahnur <57170706+gryto@users.noreply.github.com>
2022-07-12 09:51:37 +0700

    commenting app_start for pm2 :thinking:

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,7 +2,7 @@
 cd /home/ec2-user/server/src
 npm run build
 npm start
-pm2 start npm -- start
-pm2 startup
-pm2 save
-pm2 restart all
+#pm2 start npm -- start
+#pm2 startup
+#pm2 save
+#pm2 restart all
